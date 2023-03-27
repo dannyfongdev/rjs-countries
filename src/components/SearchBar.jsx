97b2e575dcc4557d2b4a63836293e4c8ede19d33
Danny Fong <danny.fong.dev@outlook.com>
2023-03-27 16:16:22 -0700

    Search bug fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,21 +12,34 @@ function Search(props) {
     onSearch(term);
   };
 
-  // @todo bug, search with 'a' causes internal server error
-  // @todo, need magnifying glass icon
-
   return (
     <div className="flex flex-col justify-between bg-lightBackground px-5 text-lightText dark:bg-darkBackground dark:text-darkText md:flex-row xl:px-[80px]">
-      <form className="mt-6 mb-6 text-sm md:text-base md:mt-12 md:mb-2" onSubmit={handleSubmit}>
+      <form
+        className="relative mt-6 mb-6 text-sm md:mt-12 md:mb-2 md:text-base"
+        onSubmit={handleSubmit}
+      >
         <label htmlFor="search"></label>
         <input
-          className="w-full rounded-md p-4 shadow focus:outline-none focus:ring-2 focus:ring-gray-300 dark:bg-darkElements dark:focus:ring-gray-700 md:w-[480px]"
+          className="w-full rounded-md p-4 pl-10 shadow focus:outline-none focus:ring-2 focus:ring-gray-300 dark:bg-darkElements dark:focus:ring-gray-700 md:w-[480px]"
           id="search"
           type="text"
           value={term}
           onChange={(e) => setTerm(e.target.value)}
           placeholder="Search for a country..."
         />
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2.0}
+          stroke="currentColor"
+          className="absolute top-5 left-4 h-4 w-4 stroke-gray-500 dark:stroke-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
       </form>
       <DropdownMenu onSelectRegion={onSelectRegion} />
     </div>
